7f3e3c0797e010b4354dc6703e10f9439973525f
Tejas Jaybhaye <117623822+tejayy@users.noreply.github.com>
2023-10-06 15:42:11 +0530

    First Commit

---
+++ src/components/App.jsx
@@ -0,0 +1,15 @@
+import Footer from './Footer';
+import Header from './Header';
+import Note from './note';
+
+const App = () => {
+    return (
+        <div>
+            <Header/>
+            <Note/>
+            <Footer/>
+        </div>
+    );
+}
+ 
+export default App;
---
+++ src/components/Footer.jsx
@@ -0,0 +1,12 @@
+const Footer = () => {
+
+    const year = new Date().getFullYear();
+
+    return (
+        <footer>
+            <p>Copyright ⓒ {year}</p>
+        </footer>
+    );
+}
+ 
+export default Footer;
